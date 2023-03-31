fa5bd64f70aefedcf9999b79567cc2e169dff391
muhammadhanif71 <muhammadhanif71@outlook.com>
2023-03-31 11:06:33 +0700

    ,+_??

--- VIPBOT/pyrainger/Ubot/modules/basic/__init__.py
+++ VIPBOT/pyrainger/Ubot/modules/basic/__init__.py
@@ -4,20 +4,12 @@ import os
 import sys
 from os import environ, execle, path, remove
 from Ubot.modules.basic.help import add_command_help
-from Ubotlibs import Ubot, DEVS BOT_VER
+from Ubotlibs import Ubot, DEVS, BOT_VER, ADMINS, BL_GCAST
 
 add_command_help = add_command_help
 
-ADMINS = [5615921474, 1620434318, 1442917841]
 
-BL_GCAST = [-1001755737234]
-
-BL_UBOT = [1245451624]
-DEVS = [
-  5615921474,
-  1620434318,
-  1442917841,
-  ]
+BL_UBOT = [-1001755737234]
 
 def restart():
     os.execvp(sys.executable, [sys.executable, "-m", "Ubot"])
